67ce6cc3b69a6b7b6c63ca7c4bf9b19023d1950e
Gurvan TANDEO <gtandeo@e3r3p21.42.fr>
2016-03-01 05:15:26 +0100

    rendu

--- estimatePrice.py
+++ estimatePrice.py
@@ -8,12 +8,14 @@ theta1 = 0
 mileage = 0
 estimated_price = 0.0
 
+#get user mileage input
 try:
 	mileage = int(raw_input("Input: "))
 except ValueError:
 	print "please enter a valid value"
 	sys.exit(0)
 
+#parse theta0 and theta1's values
 if os.path.exists("data/theta.csv") is True:
 	theta_file = open("data/theta.csv", "r")
 	tmp_line = theta_file.readline()
@@ -21,5 +23,6 @@ if os.path.exists("data/theta.csv") is True:
 	theta1 = float(tmp_line.split(" ", 2)[1])
 	theta_file.close()
 
+#calc estimate_price
 estimated_price = theta0 + (theta1 * mileage)
 print "This car worth " + str(estimated_price) + " euros"
--- train_model.py
+++ train_model.py
@@ -9,15 +9,6 @@ prices = []
 learningRate = 0.15
 theta0 = 0.0
 theta1 = 0.0
-tmptheta0 = 0.0
-tmptheta1 = 0.0
-
-if os.path.exists("data/theta.csv") is True:
-	theta_file = open("data/theta.csv", "r")
-	tmp_line = theta_file.readline()
-	theta0 = float(tmp_line.split(" ", 2)[0])
-	theta1 = float(tmp_line.split(" ", 2)[1])
-	theta_file.close()
 
 #parse data file
 try:
@@ -26,6 +17,15 @@ except IOError:
 	print "error: missing data file \'data/data.csv\'"
 	sys.exit(0)
 
+#parse theta0 and theta1's values
+if os.path.exists("data/theta.csv") is True:
+	theta_file = open("data/theta.csv", "r")
+	tmp_line = theta_file.readline()
+	theta0 = float(tmp_line.split(" ", 2)[0])
+	theta1 = float(tmp_line.split(" ", 2)[1])
+	theta_file.close()
+
+#stock data in tab
 i = 0
 for line in my_data:
 	if i > 0:
@@ -34,13 +34,19 @@ for line in my_data:
 		prices.append(int(tmp[1]))
 	i += 1
 
-#calc theta0 and theta1
-tmptheta0 = sum([learningRate * float(1) / len(mileages) * (mileages[i] - prices[i]) for i in range(0, len(mileages))])
-tmptheta1 = sum([learningRate * float(1) / len(mileages) * (mileages[i] - prices[i]) * mileages[i] for i in range(0, len(mileages))])
+#calc sigma
+sumtheta0 = sum([theta0 + (theta1 * mileages[i]) - prices[i] for i in range(0, len(mileages))])
+sumtheta1 = sum([(theta0 + (theta1 * mileages[i]) - prices[i]) * mileages[i] for i in range(0, len(mileages))])
 
+#calc theta's tmp values
+tmptheta0 = learningRate * float(1) / len(mileages) * sumtheta0
+tmptheta1 = learningRate * float(1) / len(mileages) * sumtheta1
+
+#calc final theta's values
 theta0 = theta0 - tmptheta0
 theta1 = theta1 - tmptheta1
 
+#write theta0 and theta1 in data file
 theta_file = open("data/theta.csv", "w+")
 theta_file.write(str(theta0) + ' ' + str(theta1))
 theta_file.close()
